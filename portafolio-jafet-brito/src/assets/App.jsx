7432e991891c4cbe924a8577471960402b553fa3
Jafet Brito <wjafte28@gmail.com>
2022-06-19 17:54:58 -0500

    Investigar
    Me parece que ya instalado, dado que compila. Se realizaron muchos cambios. Una instalación algo tediosa pero interesante. Necesito revisar los cambios, dado que se agregaron palabras clave muy importantes en la instalacion, me parece brutal, multiplicar un repositorio y tenerlo casi instantaneo para dirigirlo a otro framework, en el siguente caso instalaremos boostrap. Pero en este caso fue bastante util trabajar de esa manera, lo subo en una version que funciona. Es decir es inicial, de aqui tambien puede derivar algunas ramas.

---
+++ portafolio-jafet-brito/src/assets/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>tailwaind</h1>
+            <h2>Css</h2>
+        </div>
+    )
+}
+export default App
